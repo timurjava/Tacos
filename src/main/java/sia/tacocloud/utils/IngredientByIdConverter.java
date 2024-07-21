d9fce5c44bf0c9dea2a12d63cccd946bbc42e2a2
timur <timur.skotnikov@avon.com>
2024-07-21 22:50:48 +0300

    hot fix

--- src/main/java/sia/tacocloud/utils/IngredientByIdConverter.java
+++ src/main/java/sia/tacocloud/utils/IngredientByIdConverter.java
@@ -5,12 +5,12 @@ import jakarta.annotation.Resource;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import sia.tacocloud.domain.dto.Ingredient;
-import sia.tacocloud.domain.repository.Implementation.JdbcIngredientRepository;
+import sia.tacocloud.domain.repository.IngredientRepository;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
     @Resource
-    private JdbcIngredientRepository jdbcIngredientRepository;
+    private IngredientRepository jdbcIngredientRepository;
     @Override
     public Ingredient convert(String id) {
         return jdbcIngredientRepository.findById(id).orElse(null);
