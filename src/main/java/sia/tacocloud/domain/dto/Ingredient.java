c6304acef8c3035f3c7cac00d6063abc52f01182
timur <timur.skotnikov@avon.com>
2024-07-27 16:24:28 +0300

    added databse filling with aplication runner(NOW Working)

--- src/main/java/sia/tacocloud/domain/dto/Ingredient.java
+++ src/main/java/sia/tacocloud/domain/dto/Ingredient.java
@@ -1,9 +1,19 @@
 package sia.tacocloud.domain.dto;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("INGREDIENT")
-public record Ingredient(@Id String id, String name, IngredientType type) {
+public record Ingredient(@Id String id, String name, IngredientType type) implements Persistable<String> {
 
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return true;
+    }
 }
